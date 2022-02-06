6d19d291f1a8f726bb729d5764e659d51be868d3
Curt Bridgers <bridgerspc@gmail.com>
2022-02-06 13:05:26 -0500

    Added functio print_title for drawing titles to UI windows, added a color pair for those titles.

--- ui.c
+++ ui.c
@@ -145,6 +145,18 @@ print_stack(WINDOW *win)
     return;
 }
 
+/**
+ * Prints a title int the middle of the top thing.
+ * Note: call after boxing the window/drawing the window's border
+ */
+void
+print_title(WINDOW *win, int width, char *title) {
+    mvwaddstr(win, 0, (width / 2) - (strlen(title) / 2), title);
+    mvwchgat(win, 0, width / 2 - strlen(title) / 2, strlen(title),
+            A_BOLD | A_UNDERLINE, WINDOW_TITLE_COLOR, NULL);
+    return;
+}
+
 
 /**
  * The nCurses thread run function.
@@ -179,6 +191,7 @@ cthread_run(void *arg)
     init_pair(1, COLOR_GREEN, COLOR_BLACK);
     init_pair(2, COLOR_YELLOW, COLOR_BLACK);
     init_pair(3, COLOR_RED, COLOR_BLACK);
+    init_pair(4, COLOR_CYAN, COLOR_BLACK);
 
     // Window widths
     uint16_t w_src;
@@ -295,6 +308,9 @@ cthread_run(void *arg)
         box(window_out, 0, 0);
         box(window_src, 0, 0);
         box(window_mem, 0, 0);
+        print_title(window_out, w_out, "Program Output");
+        print_title(window_src, w_src, "Vulnerable Code");
+        print_title(window_mem, w_mem, "Stack Memory Dump");
 
         update_panels();
         doupdate();
@@ -355,6 +371,7 @@ get_user_input()
     // Draw form window and post form
     top_panel(pan_form);
     box(win_form, 0, 0);
+    print_title(win_form, w_form, "Enter an input string");
     post_form(form);
 
     update_panels();
